45e319bea9d9a8569380d9824f93b2c8ec8b31a2
mauricio gutierrez <cirus55@gmail.com>
2021-01-21 13:14:49 -0600

    pagina administrador

--- manager.js
+++ manager.js
@@ -1,3 +1,26 @@
+$('#createUserForm').hide();
+$('#manageExpensesContainer').hide();
+
+//-----------------------------------------------
+
+$(document).on("click", "#createUser", showCreateUserForm);
+
+// function to create user
+function showCreateUserForm() {
+    $('#createUserForm').show();
+    $('#manageExpensesContainer').hide();
+};
+
+$(document).on("click", "#manageExpenses", showManageExpensesContainer);
+
+// function to create user
+function showManageExpensesContainer() {
+    $('#createUserForm').hide();
+    $('#manageExpensesContainer').show();
+};
+
+//-----------------------------------------------
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // window.location.replace("manager.html");
